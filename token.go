d8a0a91f521f4a94547e3a47b82d216018c71d61
Konstantinos Vlachopoulos <kon@vlach.gr>
2024-07-02 17:04:07 +0300

    Add TokenList

---
+++ token.go
@@ -0,0 +1,18 @@
+package bricked
+
+type TokenList struct {
+	TokenInfos []struct {
+		TokenID      string `json:"token_id"`
+		CreationTime int64  `json:"creation_time"`
+		ExpiryTime   int64  `json:"expiry_time"`
+		Comment      string `json:"comment"`
+	} `json:"token_infos"`
+}
+
+func (c *client) TokenList() TokenList {
+	c.assertVersion("2.0")
+
+	var resp TokenList
+	c.GET("token/list", &resp)
+	return resp
+}
